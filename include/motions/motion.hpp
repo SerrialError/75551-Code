edbfcb434e8ec54dd76c8c95c3b369f8a777eb43
Serrial Error <serrialerror@outlook.com>
2026-02-26 22:13:26 -0800

    added and fixed match autos in jerryio

---
+++ include/motions/motion.hpp
@@ -0,0 +1,22 @@
+#ifndef MOTION_HPP
+#define MOTION_HPP
+
+#include <memory>
+
+class Plant; // forward
+
+class Motion {
+public:
+    enum class Result { Running, Completed, Cancelled };
+    virtual ~Motion() = default;
+
+    virtual void onStart() {}
+
+    virtual Result step() = 0;
+
+    virtual void onEnd() {}
+private:
+	std::weak_ptr<Plant> plant;
+};
+
+#endif // MOTION_HPP
