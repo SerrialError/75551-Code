0994a8571bba609df28056a154cb05d760c639dd
connor-hofland <chofland0254@bghscloud.org>
2026-04-15 17:18:29 -0700

    fixed port

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -10,7 +10,7 @@ inline pros::adi::Pneumatics match_loader ('h', false, false);
 inline pros::adi::Pneumatics wings ('f', false, false);
 inline pros::adi::Pneumatics two_state_system ('b', false, false);
 inline pros::Motor front(-4, pros::v5::MotorGears::blue);
-inline pros::Motor back(-14, pros::v5::MotorGears::blue);
+inline pros::Motor back(14, pros::v5::MotorGears::blue);
 
 inline const rollers<std::reference_wrapper<pros::Motor>> intakeMotors{
 	std::ref(front),
